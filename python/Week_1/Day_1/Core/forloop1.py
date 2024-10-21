3a33c35f23804b153171a9ce853f7ef5d835c236
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-10-21 18:04:33 +0100

    function basics 1

---
+++ python/Week_1/Day_1/Core/forloop1.py
@@ -0,0 +1,33 @@
+#1
+for i in range(0,151) :
+    print(i)
+#2
+for i in range(0,1001):
+    if i%5==0:
+        print(i)
+#3
+for i in range(0,101):
+    if i%5==0:
+        print("coding")
+    if i%10==0:
+        print ("codin dojo")
+    else:
+        print(i)
+#4
+sum=0
+for i in range(0,5000001):
+    if i % 2!=0:
+        sum=sum+i
+print(sum)
+#5
+for i in range(2018,-1,-4):
+    print(i)
+
+
+#6
+lownum=2
+highnum=9
+mult=3
+for i in range (lownum+1,highnum+1,mult):
+    print(i)
+    
