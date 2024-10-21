e7afcd53a1eac2a0ee77040ede44b952390fa35c
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-10-21 15:59:22 +0100

    hello world

---
+++ python/Week_1/Day_1/Practice/helloworld.py
@@ -0,0 +1,22 @@
+#1 
+print("hello world")
+#2a
+name="aziz"
+print("hello", name ,"!")
+#2b
+name="aziz"
+print("hello " + name + " !")
+#3a
+num=5
+print("hello ",num)
+#3b
+num=5
+print("hello "+ str(num))
+#4a
+food1="bread"
+food2="pizza"
+print("i love to eat",food1,"and",food2)
+#4b
+food1="bread"
+food2="pizza"
+print(f"i love to eat {food1} and {food2} ")
